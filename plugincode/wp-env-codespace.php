d625f69d5a5c075054554b132b9e59ff164e0192
Thomas Rose <mail@tom-rose.de>
2023-09-27 19:12:42 +0000

    add demo plugin

---
+++ plugincode/wp-env-codespace.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This plugin is just for demonstrating.
+ *
+ * @package WP_Env_Codespace
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: WP-Env Codespace
+Description: This plugin is just for demonstrating.
+Author: Tom Rose
+*/
+
+
+/**
+ * Adds a Hello Codespace to the admin area.
+ */
+function wp_codespace_notices() {
+	echo '<p class="wp-codespace">Hello codespace!</p>';
+}
+add_action( 'admin_notices', 'wp_codespace_notices' );
+
+/**
+ * Register and enqueue a stylesheet in admin.
+ */
+function wp_codespace_enqueue_admin_style() {
+		wp_register_style( 'wp_codespace_admin_style', plugin_dir_url( __FILE__ ) . 'styles.css', null, false, '1.0.0' );
+		wp_enqueue_style( 'wp_codespace_admin_style' );
+}
+add_action( 'admin_enqueue_scripts', 'wp_codespace_enqueue_admin_style' );
