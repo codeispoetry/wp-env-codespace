1d252856f7029dfcf852a36140863aef82b9434b
Thomas Rose <thomas.rose@ionos.com>
2023-09-28 07:54:24 +0200

    add auto codespace name

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,16 @@
+PORT=80
+
+cat <<EOF > ../.wp-env.override.json
+{
+    "config" : {
+        "WP_HOME" : "${CODESPACE_NAME}-${PORT}${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+	}
+}
+EOF
+
 npm install
 composer install
-npm run up
+# npm run up
+
+
 
