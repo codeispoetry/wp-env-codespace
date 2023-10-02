1b4e1d2b48bb894342a1dc489c7c7b55e8981e14
Thomas Rose <mail@tom-rose.de>
2023-10-02 10:10:43 +0000

    fix url in overwrite

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,7 @@ OUTPUT_FILE="$(dirname "$(readlink -f "$0")")/../.wp-env.override.json"
 cat <<EOF > $OUTPUT_FILE
 {
     "config" : {
-        "WP_HOME" : "${CODESPACE_NAME}-${PORT}${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+        "WP_HOME" : "https://${CODESPACE_NAME}-${PORT}${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 	}
 }
 EOF
